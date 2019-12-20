d51dc2758753059410232d16f86f03025033849e
callcocam <callcocam@gmail.con>
2019-12-20 18:58:30 -0300

    campos de uploads

--- resources/views/admin/profile/show.blade.php
+++ resources/views/admin/profile/show.blade.php
@@ -11,7 +11,7 @@
 @section('content')
     <div class="card user-profile o-hidden mb-4">
         <div class="header-cover" style="background-image: url({{ asset('_dist/admin/images/photo-wide-4.jpg') }})"></div>
-        <div class="user-info"><img class="profile-picture avatar-lg mb-2" src="{{ asset($rows->avatar) }}" alt="" id="image_tag"/>
+        <div class="user-info"><img class="profile-picture avatar-lg mb-2" src="{{ url($rows->avatar) }}" alt="" id="image_tag"/>
             <p class="m-0 text-24">{{ $rows->name }}</p>
             <p class="text-muted m-0">{{ $rows->email }}</p>
         </div>
--- resources/views/admin/settings/show.blade.php
+++ resources/views/admin/settings/show.blade.php
@@ -10,7 +10,7 @@
 @endsection
 @section('content')
     <div class="card user-profile o-hidden mb-4">
-        <div class="header-cover m-3" style="background-image: url({{ asset($rows->cover) }});background-size: contain;background-position: top;"></div>
+        <div class="header-cover m-3" style="background-image: url({{ url($rows->cover) }});background-size: contain;background-position: top;"></div>
         <div class="card-body">
 
             <div class="row">
