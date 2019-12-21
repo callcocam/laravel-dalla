bc60bb7b13c0c33a7519cf8f9823069594700098
callcocam <callcocam@gmail.con>
2019-12-21 18:29:03 -0300

    campos de uploads

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -1,10 +1,10 @@
 <div class="side-content-wrap">
     <div class="sidebar-left open rtl-ps-none" data-perfect-scrollbar="" data-suppress-scroll-x="true">
         <ul class="navigation-left">
-                <li class="nav-item">
-                    <a class="nav-item-hold" href="{{ route('admin.admin.index') }}"><i class="nav-icon i-Dashboard"></i><span class="nav-text">{{ __('Dashboard') }}</span></a>
-                    <div class="triangle"></div>
-                </li>
+            <li class="nav-item">
+                <a class="nav-item-hold" href="{{ route('admin.admin.index') }}"><i class="nav-icon i-Dashboard"></i><span class="nav-text">{{ __('Dashboard') }}</span></a>
+                <div class="triangle"></div>
+            </li>
             @can('admin.settings.index')
                 <li class="nav-item">
                     <a class="nav-item-hold" href="{{ route('admin.settings.setting') }}"><i class="nav-icon i-Gear-2"></i><span class="nav-text">{{ __('Settings') }}</span></a>
@@ -34,20 +34,28 @@
     <div class="sidebar-left-secondary rtl-ps-none" data-perfect-scrollbar="" data-suppress-scroll-x="true">
         <!-- Submenu Dashboards-->
         <ul class="childNav" data-parent="operacional">
-            @can('admin.users.index')
-                <li class="nav-item"><a href="{{ route('admin.users.index') }}"><i class="nav-icon i-Arrow-Forward-2"></i><span class="item-name">{{ __('Users') }}</span></a></li>
-            @endcan
-            @can('admin.permissions.index')
-                <li class="nav-item"><a href="{{ route('admin.permissions.index') }}"><i class="nav-icon i-Arrow-Forward-2"></i><span class="item-name">{{ __('Permissions') }}</span></a></li>
-            @endcan
-            @can('admin.roles.index')
-                <li class="nav-item"><a href="{{ route('admin.roles.index') }}"><i class="nav-icon i-Arrow-Forward-2"></i><span class="item-name">{{ __('Roles') }}</span></a></li>
-            @endcan
+            @if (Route::has('admin.users.index'))
+                @can('admin.users.index')
+                    <li class="nav-item"><a href="{{ route('admin.users.index') }}"><i class="nav-icon i-Arrow-Forward-2"></i><span class="item-name">{{ __('Users') }}</span></a></li>
+                @endcan
+            @endif
+            @if (Route::has('admin.permissions.index'))
+                @can('admin.permissions.index')
+                    <li class="nav-item"><a href="{{ route('admin.permissions.index') }}"><i class="nav-icon i-Arrow-Forward-2"></i><span class="item-name">{{ __('Permissions') }}</span></a></li>
+                @endcan
+            @endif
+            @if (Route::has('admin.roles.index'))
+                @can('admin.roles.index')
+                    <li class="nav-item"><a href="{{ route('admin.roles.index') }}"><i class="nav-icon i-Arrow-Forward-2"></i><span class="item-name">{{ __('Roles') }}</span></a></li>
+                @endcan
+            @endif
         </ul>
         <ul class="childNav" data-parent="events">
-            @can('admin.events.index')
-                <li class="nav-item"><a href="{{ route('admin.events.index') }}"><i class="nav-icon i-Arrow-Forward-2"></i><span class="item-name">{{ __('List') }}</span></a></li>
-            @endcan
+            @if (Route::has('admin.events.index'))
+                @can('admin.events.index')
+                    <li class="nav-item"><a href="{{ route('admin.events.index') }}"><i class="nav-icon i-Arrow-Forward-2"></i><span class="item-name">{{ __('List') }}</span></a></li>
+                @endcan
+            @endif
         </ul>
 
     </div>
