735350d4d2c1f4f5943f3809b06c5fa5dc7574c2
callcocam <callcocam@gmail.con>
2019-12-23 12:04:03 -0300

    main header

--- resources/views/admin/includes/main-header.blade.php
+++ resources/views/admin/includes/main-header.blade.php
@@ -8,24 +8,4 @@
         <div></div>
     </div>
     <div style="margin: auto"></div>
-    <div class="header-part-right">
-        <!-- Full screen toggle
-        <i class="i-Full-Screen header-icon d-none d-sm-inline-block" data-fullscreen></i>-->
-        <!-- Grid menu Dropdown -->
-
-        <!-- User avatar dropdown -->
-        <div class="dropdown">
-            <div class="user col align-self-end">
-                <img src="{{ url($user->avatar) }}" id="userDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-                    <div class="dropdown-header">
-                        <i class="i-Lock-User mr-1"></i> {{ $user->name }}
-                    </div>
-                    <a class="dropdown-item"  href="{{ route('admin.auth.profile.form') }}">{{ __("Minha Conta") }}</a>
-                    <a class="dropdown-item" href="#" onclick="event.preventDefault();
-                       document.getElementById('logout-form').submit();">{{ __('Sair') }}</a>
-                </div>
-            </div>
-        </div>
-    </div>
 </div>
