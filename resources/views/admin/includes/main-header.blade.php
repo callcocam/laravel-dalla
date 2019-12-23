b32a623aacf4fd0a74934a97370c92803421aabd
callcocam <callcocam@gmail.con>
2019-12-23 11:59:23 -0300

    main header

--- resources/views/admin/includes/main-header.blade.php
+++ resources/views/admin/includes/main-header.blade.php
@@ -16,7 +16,7 @@
         <!-- User avatar dropdown -->
         <div class="dropdown">
             <div class="user col align-self-end">
-                <img src="{{ asset($user->cover) }}" id="userDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <img src="{{ url($user->cover) }}" id="userDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
                     <div class="dropdown-header">
                         <i class="i-Lock-User mr-1"></i> {{ $user->name }}
