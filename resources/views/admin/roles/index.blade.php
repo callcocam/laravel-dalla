f42bc5012fbdf2e5274f628ceddbd0d940b5eceb
callcocam <callcocam@gmail.con>
2020-01-16 11:09:31 -0300

    roles list

--- resources/views/admin/roles/index.blade.php
+++ resources/views/admin/roles/index.blade.php
@@ -36,10 +36,10 @@
                                         <li class="list-group-item border-0">
                                             @foreach($row->permissions as $permission)
                                                 @can('admin.permissions.edit')
-                                                    <a href="{{ route('admin.permissions.edit', $role->id) }}" class="badge badge-{{ get_tag_color() }} r-badge">{{ $role->name }}</a>
+                                                    <a href="{{ route('admin.permissions.edit', $permission->id) }}" class="badge badge-{{ get_tag_color() }} r-badge">{{ $permission->name }}</a>
                                                 @endcan
                                                 @cannot('admin.permissions.edit')
-                                                    <span class="badge badge-{{ get_tag_color() }} r-badge">{{ $role->name }}</span>
+                                                    <span class="badge badge-{{ get_tag_color() }} r-badge">{{ $permission->name }}</span>
                                                 @endcannot
                                             @endforeach
                                         </li>
